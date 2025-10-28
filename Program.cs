08ab232b97ed2c7507a7bf241f6351238423914e
mateusjesus001 <149727850+mateusjesus001@users.noreply.github.com>
2025-10-28 08:18:17 -0300

    Configurando o projecto com Entity Framework e mesa de administradores

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-var app = builder. Build();
+builder.Services. AddDbContext<DbContexto>(options =>
+{
+  options.UseMySql(
+   builder.Configuration.GetConnectionString("mysql"),
+   ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+  );
+});
+var app = builder.Build();
 app.MapGet("/", () => "Olá pessoal");
-app. MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
+app. MapPost("/login", (LoginDTO loginDTO) => {
  if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha
 == "123456")
    return Results.Ok("Login com sucesso");
