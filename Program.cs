66a58043451ecdef1e929f15c98f0c863d69b288
mateusjesus001 <149727850+mateusjesus001@users.noreply.github.com>
2025-10-30 10:22:32 -0300

    Metodos dos veiculos implementados

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ var app = builder.Build();
 #endregion
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home()));
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
 #region Administradores
@@ -38,7 +38,7 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
     return Results.Ok("Login com sucesso");
   else
     return Results.Unauthorized();
-});
+}).WithTags("Administradores");
 #endregion
 
 #region Veiculos
@@ -52,15 +52,47 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
   };
   veiculoServico.Incluir(veiculo);
   return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
-});
+}).WithTags("Veiculos");
 
 app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
 {
   var veiculos = veiculoServico.Todos(pagina);
 
   return Results.Ok(veiculos);
-});
-#endregion
+}).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
+  var veiculo = veiculoServico.BuscaPorId(id);
+  if (veiculo == null) return Results.NotFound();
+  return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
+app.MapPut("/veiculos/{id}", ([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+  var veiculo = veiculoServico.BuscaPorId(id);
+  if (veiculo == null) return Results.NotFound();
+
+  veiculo.Nome = veiculoDTO.Nome;
+  veiculo.Marca = veiculoDTO.Marca;
+  veiculo.Ano = veiculoDTO.Ano;
+
+  veiculoServico.Atualizar(veiculo);
+
+  return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
+app.MapDelete("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
+  var veiculo = veiculoServico.BuscaPorId(id);
+  if (veiculo == null) return Results.NotFound();
+ 
+
+  veiculoServico.Apagar(veiculo);
+
+  return Results.NoContent();
+}).WithTags("Veiculos");
+#endregion'
 
 #region App
 app.UseSwagger();
