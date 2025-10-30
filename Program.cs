8b618a22bfc8534dffcbcfa22018beeb6d0c1b12
mateusjesus001 <149727850+mateusjesus001@users.noreply.github.com>
2025-10-30 07:26:11 -0300

    Criando rota Home respondendo por Json

--- Program.cs
+++ Program.cs
@@ -3,11 +3,15 @@ using MinimalApi.Dominio.Interfaces;
 using MinimalApi.Infraestrutura.Db;
 using Microsoft.AspNetCore.Mvc;
 using MinimalApi.DTOs;
+using MinimalApi.Dominio.ModelViews;
 using MinimalApi.Dominio.Servicos;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services. AddDbContext<DbContexto>(options =>
 {
@@ -17,12 +21,18 @@ builder.Services. AddDbContext<DbContexto>(options =>
   );
 });
 var app = builder.Build();
-app.MapGet("/", () => "Olá pessoal");
-app. MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
- if(administradorServico.Login(loginDTO) != null)
-   return Results.Ok("Login com sucesso");
- else
-   return Results. Unauthorized();
+
+
+app.MapGet("/", () => Results.Json(new Home()));
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+{
+  if (administradorServico.Login(loginDTO) != null)
+    return Results.Ok("Login com sucesso");
+  else
+    return Results.Unauthorized();
 });
+
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
 
